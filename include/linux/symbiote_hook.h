c5fe1720160e4add7be5f384044bdbc13639a2e4
Fredrik Wilke <fwilke@bu.edu>
2025-09-05 16:45:18 -0400

    [fix:] revert logging

--- include/linux/symbiote_hook.h
+++ include/linux/symbiote_hook.h
@@ -7,14 +7,6 @@ struct SymbiReg; /* your existing type */
 typedef void (*symbi_hook_t)(struct pt_regs *regs,
                              const struct SymbiReg *sreg);
 
-unsigned long srrar_start;
-unsigned long bef_ker_ds;
-unsigned long bef_rsp_set;
-
-
-EXPORT_SYMBOL_GPL(srrar_start);
-EXPORT_SYMBOL_GPL(bef_ker_ds);
-EXPORT_SYMBOL_GPL(bef_rsp_set);
 
 /* Register/unregister the single global hook */
 int  symbi_register_hook(symbi_hook_t fn);
